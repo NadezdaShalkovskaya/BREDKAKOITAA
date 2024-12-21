df2ac67b83c90bb784307c8b9b136a5a3e935426
Надежда <@DESKTOP-EHSPKBR>
2024-12-21 21:55:17 +0200

    Добавьте файлы проекта.

---
+++ BREDKAKOITAA/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        DateTime dzte = DateTime.Now;
+        string formattedDate = ("{date.Day}{date.ToString(dddd)}{date.ToString(MMMM)}{date.Year}");
+        Console.WriteLine("Сегодня:" + formattedDate);
+        Console.WriteLine("Введите время старта (ЧЧ:ММ:СС):");
+        TimeSpan startTime = TimeSpan.Parse(Console.ReadLine());
+        Console.WriteLine("Введите время финиша(ЧЧ:ММ:СС):");
+        TimeSpan finishTime = TimeSpan.Parse(Console.ReadLine());
+        TimeSpan duraction = finishTime - startTime;
+        double totalSeconds = duraction.TotalSeconds;
+        double distance = 3000;
+        double averageSpeed = distance / totalSeconds;
+        Console.WriteLine($"Время прохождения дистанции:{totalSeconds}секунд");
+        Console.WriteLine($"Средняя скорость:{averageSpeed} м/с");
+    }
+}
